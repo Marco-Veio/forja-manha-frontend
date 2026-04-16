7af94e3c7a7800ee6bf1bf6ebafcaefd240ae6a1
Marco Véio <marcoarthomaz@gmail.com>
2026-04-16 08:05:28 -0300

    formulário de login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,13 @@
+import LoginForm from "@/components/LoginForm";
+
+import { loginAction } from "./actions";
+
 export default function LoginPage() {
   return (
     <div className="w-screen h-screen flex flex-col items-center">
       <h1 className="mt-10 mb-50 text-5xl font-bold">Login</h1>
 
-      <div className="w-[90%] h-100 bg-white rounded-md shadow-md shadow-white text-black py-28 px-10 overflow-auto flex flex-col items-center justify-between">
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full border border-black"
-        />
-        <input
-          type="password"
-          placeholder="Senha"
-          className="w-full border border-black"
-        />
-
-        <button className="bg-black text-white rounded-xl px-10 py-2">
-          Entrar
-        </button>
-      </div>
+      <LoginForm onSend={loginAction} />
     </div>
   );
 }
