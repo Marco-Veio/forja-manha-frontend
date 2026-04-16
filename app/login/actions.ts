a284586609e25c1f3ab1d872651fcdb6bd5a450e
Marco Véio <marcoarthomaz@gmail.com>
2026-04-16 13:16:04 -0300

    middleware

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { cookies } from "next/headers";
+
 export async function loginAction(email: string, password: string) {
   const response = await fetch("http://localhost:8080/funcionarios/login", {
     method: "POST",
@@ -10,7 +12,15 @@ export async function loginAction(email: string, password: string) {
       email,
       senha: password,
     }),
-  }).then((res) => res.json());
+  });
 
-  console.log(response);
+  const data = await response.json();
+
+  if (response.status === 200) {
+    const cookiesStore = await cookies();
+    cookiesStore.set("access_token", data.access_token);
+    return;
+  }
+
+  return data;
 }
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,14 +1,27 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 interface Props {
-  onSend: (email: string, password: string) => void;
+  onSend: (email: string, password: string) => Promise<void | string>;
 }
 
 export default function LoginForm({ onSend }: Props) {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  async function handleSubmit() {
+    const response = await onSend(email, password);
+
+    if (response) {
+      alert(response);
+      return;
+    }
+
+    router.push("/");
+  }
 
   return (
     <div className="w-[90%] h-100 bg-white rounded-md shadow-md shadow-white text-black py-28 px-10 overflow-auto flex flex-col items-center justify-between">
@@ -29,7 +42,7 @@ export default function LoginForm({ onSend }: Props) {
 
       <button
         className="bg-black text-white rounded-xl px-10 py-2 cursor-pointer hover:opacity-80"
-        onClick={() => onSend(email, password)}
+        onClick={handleSubmit}
       >
         Entrar
       </button>
