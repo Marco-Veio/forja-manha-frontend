327b2fbf9313ff7aaf93c277435e6cc57ff763de
Marco Véio <marcoarthomaz@gmail.com>
2026-05-04 09:40:45 -0300

    finalizando editar aluno

--- app/aluno/[id]/editar/page.tsx
+++ app/aluno/[id]/editar/page.tsx
@@ -1,25 +1,41 @@
 "use client";
 
 import { Aluno } from "@/interfaces/alunos";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { getAluno } from "../actions";
+import { useParams, useRouter } from "next/navigation";
+import { SubmitEvent, useEffect, useState } from "react";
+import { getAluno, updateAluno } from "../actions";
 
 export default function AlunoPage() {
   const { id } = useParams();
   const [aluno, setAluno] = useState({} as Aluno);
+  const router = useRouter();
 
   useEffect(() => {
     getAluno(Number(id)).then((response) => setAluno(response));
   }, [id]);
 
-  function handleChange(value: string, key: keyof Aluno) {
+  function handleChange(value: string | number, key: keyof Aluno) {
     setAluno((oldState) => ({ ...oldState, [key]: value }));
+  }
+
+  async function handleUpdate(e: SubmitEvent) {
+    e.preventDefault();
+    const response = await updateAluno(Number(id), aluno);
+
+    if (response) {
+      alert(response);
+      return;
+    }
+
+    router.push(`/aluno/${id}`);
   }
 
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <form className="p-6 bg-white text-black rounded-lg shadow shadow-white max-w-md flex flex-col items-center gap-2">
+      <form
+        className="p-6 bg-white text-black rounded-lg shadow shadow-white max-w-md flex flex-col items-center gap-2"
+        onSubmit={handleUpdate}
+      >
         <input
           value={aluno.nome}
           onChange={(e) => handleChange(e.target.value, "nome")}
@@ -28,7 +44,13 @@ export default function AlunoPage() {
         <input
           value={aluno.cpf}
           type="number"
-          onChange={(e) => handleChange(e.target.value, "cpf")}
+          onChange={(e) => handleChange(Number(e.target.value), "cpf")}
+          className="w-full border border-black px-1"
+        />
+        <input
+          value={aluno.idade}
+          type="number"
+          onChange={(e) => handleChange(Number(e.target.value), "idade")}
           className="w-full border border-black px-1"
         />
         <input
